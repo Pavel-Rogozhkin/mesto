abdaceb8f7e67d00b30378bc44fe8e8c161e5397
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-04-02 11:01:09 +0300

    add JScript of popup

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+let PopupWindow = document.querySelector(".popup");
+let ClosePopup = PopupWindow.querySelector(".popup__close");
+let EditButton = document.querySelector(".profile_button_edit");
+let PopupContent = document.querySelector(".popup__content");
+let NameInput = document.querySelector(".popup__name");
+let ProfileInput = document.querySelector(".popup__profile");
+let SubmitButton = document.querySelector(".popup__submit");
+let ProfileTitle = document.querySelector(".profile__title");
+let ProfileDescr = document.querySelector(".profile__description");
+
+function togglePopupWindow {
+  PopupWindow.classList.toggle("popup__active");
+  NameInput.value = ProfileTitle.textContent;
+  ProfileInput.value = ProfileDescr.textContent; 
+}
+
+function SubmitHandler (event) {
+  event.preventDefault();
+  ProfileTitle.textContent = NameInput.value;
+  ProfileDescr.textContent = ProfileInput.value;
+  togglePopupWindow();
+}
+
+
+EditButton.addEventListener("click", togglePopupWindow);
+ClosePopup.addEventListener("click", togglePopupWindow);
+PopupContent.addEventListener("submit", SubmitHandler);
+SubmitButton.addEventListener("click", SubmitHandler);
