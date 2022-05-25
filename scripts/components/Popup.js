dd3275641b27e0c887a302aac20612f2a3ae842e
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-05-25 12:04:38 +0300

    add template of class Popup

---
+++ scripts/components/Popup.js
@@ -0,0 +1,22 @@
+export default class Popup {
+
+  constructor(popupSelector) {
+    this._popupSelector = popupSelector;
+  }
+
+  open() {
+
+  }
+
+  close() {
+
+  }
+
+  _handleEscClose() {
+
+  }
+
+  setEventListeners() {
+    
+  }
+}
