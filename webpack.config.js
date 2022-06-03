fbf10036aa1ae17d06a2c82583575771d26702e1
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-06-03 21:54:27 +0300

    config webpack html js1

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/pages/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "main.js",
+    publicPath: ''
   },
   devServer: {
-    port: 3000,
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 7777,
     compress: true,
-    hot: true
+    hot: true,
+    open: true
   },
   devtool: "source-map",
   mode: "development",
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         use: "babel-loader",
         exclude: "/node_modules/"
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
