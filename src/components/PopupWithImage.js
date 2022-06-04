f9e57749d043272b249c50d2bcc0a4a8adbbacb7
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-06-04 14:22:07 +0300

    refactoring1

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -15,18 +15,4 @@ export class PopupWithImage extends Popup {
     super.open();
   };
 
-  setEventListeners() {
-    this._popupElement.querySelector(".popup__close").addEventListener("click", () => { this.close(); });
-    this._popupElement.addEventListener("mousedown", (evt) => {
-      if (evt.target.classList.contains("popup")) {
-        this.close();
-      };
-    });
-    document.addEventListener('keydown', (evt) => { this._handleEscClose(evt); });
-  };
-
-  close() {
-    this._popupElement.classList.remove("popup_opened");
-  };
-
 };
